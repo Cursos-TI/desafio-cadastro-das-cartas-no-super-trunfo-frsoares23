fe4362135beece01852bd4a06f1ef369be70bee2
frsoares23 <frsoares92@outlook.com.br>
2025-02-04 20:27:17 +0000

    Projeto reiniciado

---
+++ DesafioSuperTrunfoNovato/CartasSTFabi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// Desafio Super Trunfo Países - (Nível Novato)
+
+int main(){
+    char país[], estados[];
+    int cidades, pontos turísticos;
+    double população;
+    float área;
+    double pib;
+
+    printf("Nome do País: \n");
+    scanf("%c", &país);
+
+    printf("Estado: \n");
+        if("Escolha uma letra entre A até H: \n");
+    scanf("%c", &estados, &escolha);
+    
+
+
+}
